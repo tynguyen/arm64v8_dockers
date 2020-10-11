56bf228d99354a21aabc26ccfa5eb6719f5e6df7
tyunist <tynguyen@unist.ac.kr>
2020-10-10 20:02:52 -0400

    Add commit '

--- create_docker_container.sh
+++ create_docker_container.sh
@@ -1,2 +1,8 @@
-docker run -it --privileged --net=host --name tynguyen_docker -v /dev/ptmx:/opt/ptmx -v /data/:/root/yoctohome/:rw -w /root/ arm64v8/melodic:tmux /bin/bash
+docker run -it --privileged \
+  --net=host --name tynguyen_docker \
+  -v /dev/ptmx:/opt/ptmx \
+  -v /data/:/root/yoctohome/:rw \
+  -w /root/ \
+  arm64v8/ros:tmux \
+  /bin/bash
 
